76a3c2b989d14bd6340ee34ca2ae1715e89b4bcb
ShiroNeko <103792247+Shiro-N3ko@users.noreply.github.com>
2022-09-02 13:21:54 -0400

    Add project files.

---
+++ CardGame/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public enum house
+    {
+        Spade,
+        Club,
+        Diamond,
+        Heart
+    }
+
+    public class Card
+    {
+        private Card(house house, string? value, string? colour)
+        {
+            this.house = house;
+            this.value = value;
+            this.colour = colour;
+            //this.spritePath = spritePath;
+        }
+
+        private house house { get; }
+        private string? value { get; }
+        private string? colour { get; }
+        private string? spritePath { get; set; }
+    }
+}
